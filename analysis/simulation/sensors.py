c63cf521761257d1ccd2d90c5ca813d8f9e4c5d3
Spencer Hallyburton <20426598+roshambo919@users.noreply.github.com>
2024-03-01 11:37:55 -0500

    WIP on adding fp/fn model

--- analysis/simulation/sensors.py
+++ analysis/simulation/sensors.py
@@ -1,5 +1,5 @@
 import itertools
-from typing import Tuple
+from typing import Tuple, TYPE_CHECKING
 
 import numpy as np
 from avstack.config import GEOMETRY, MODELS, ConfigDict
@@ -20,8 +20,10 @@ class SensorModel:
         extent,
         fov,
         ID=None,
-        Pd=0.95,
-        Dfa=1e-6,
+        Pd: float=0.95,
+        Dfa: float=1e-6,
+        p_p_FP: float=0,
+        p_p_FN: float=0,
         name="sensor",
     ) -> None:
         """Base class for an observation model
@@ -38,6 +40,10 @@ class SensorModel:
         self.q = q
         self.Pd = Pd
         self.Dfa = Dfa
+        self.p_p_FP = p_p_FP
+        self.p_p_FN = p_p_FN
+        self._last_fp = []
+        self._last_fn = {}
         self.extent = extent
         self.fov = GEOMETRY.build(fov)
         self._fov_area_uniform = self.fov.radius**2
@@ -48,19 +54,7 @@ class SensorModel:
         return self._reference
 
     def __call__(self, frame, timestamp, objects):
-        # -- add false positives via rejection sampling on a rectangle
-        n_fp = np.random.poisson(self.Dfa * self._fov_area_uniform)
-        x = np.random.uniform(low=-self.fov.radius, high=self.fov.radius, size=n_fp)
-        y = np.random.uniform(low=-self.fov.radius, high=self.fov.radius, size=n_fp)
-        z = np.zeros((n_fp,))
-        vs = np.column_stack((x, y, z))
-        if len(vs) > 0:
-            fov_test_fps = self.fov.check_point(vs.T)
-            objs_fp = [Position(v, self._reference) for v in vs[fov_test_fps, :]]
-        else:
-            objs_fp = []
-
-        # -- check objects in fov
+        # filter objs in FOV
         obj_xs = np.array(
             [
                 (obj.position.change_reference(self._reference, inplace=False)).x
@@ -68,17 +62,44 @@ class SensorModel:
             ]
         )
         fov_test_objs = self.fov.check_point(obj_xs.T)
-        obj_in_view = [obj for obj, in_fov in zip(objects, fov_test_objs) if in_fov]
+        objs_in_view = [obj for obj, in_fov in zip(objects, fov_test_objs) if in_fov]
+        IDs = {obj.ID for obj in objs_in_view}
+        
+        # remove false negatives with persistence
+        self._last_fn = {ID for ID in self._last_fn if ID in IDs}
+        n_fn_persist = np.random.binomial(n=len(self._last_fn), p=self.p_p_FN)
+        n_fn_new = max(0, np.random.binomial(len(objs_in_view)-n_fn_persist, p=1-self.p_D))
+        fns_persist = np.random.choice(self._last_fn, size=n_fn_persist, replace=False)
+        fns_new = np.random.choice(IDs.difference(fns_persist), size=n_fn_new, replace=False)
+        fns = fns_persist + fns_new
+        for idx in sorted(np.argwhere(IDs == fns), reverse=True):
+            del objs_in_view[idx]
+
+        # add false positives with persistence
+        n_fp_persist = np.random.binomial(n=len(self._last_fp), p=self.p_p_FP)
+        n_fp_new = max(0, np.random.poisson(self.lambda_fp(self.fov)) - n_fp_persist)
+        fps_persist = np.random.choice(self._last_fp, size=n_fp_persist, replace=False)
+        fps_new = [Position(self.fov.sample_point(), self._reference) for _ in range(n_fp_new)]
+        fps = fps_persist + fps_new
+        objs_in_view += fps
+        self._last_fp = fps
+
+        # # -- add false positives via rejection sampling on a rectangle
+        # n_fp = np.random.poisson(self.Dfa * self._fov_area_uniform)
+        # x = np.random.uniform(low=-self.fov.radius, high=self.fov.radius, size=n_fp)
+        # y = np.random.uniform(low=-self.fov.radius, high=self.fov.radius, size=n_fp)
+        # z = np.zeros((n_fp,))
+        # vs = np.column_stack((x, y, z))
+        # if len(vs) > 0:
+        #     fov_test_fps = self.fov.check_point(vs.T)
+        #     objs_fp = [Position(v, self._reference) for v in vs[fov_test_fps, :]]
+        # else:
+        #     objs_fp = []
 
         # -- make measurements
         detections = []
-        for i, objs in enumerate((obj_in_view, objs_fp)):
-            for obj in objs:
-                if not isinstance(obj, Position):
-                    obj = obj.position
-                if (i == 0) and (np.random.rand() > self.Pd):
-                    continue  # sometimes false negative
-                detections.append(self.observe(self, obj))
+        for obj in enumerate(objs_in_view):
+            detections.append(self.observe(self, obj))
         return DataContainer(frame, timestamp, detections, self.ID)
 
     def observe(self, sensor, obj, noisy):
