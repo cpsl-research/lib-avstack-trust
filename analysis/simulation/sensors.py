6ca3d96b389887ca260aca06d9b0a43817ff1bb8
Spencer H <spencer.hallyburton@duke.edu>
2024-03-04 09:38:25 -0500

    Add fp and fn with persistence models

--- analysis/simulation/sensors.py
+++ analysis/simulation/sensors.py
@@ -1,5 +1,11 @@
 import itertools
-from typing import Tuple, TYPE_CHECKING
+from typing import TYPE_CHECKING, Tuple
+
+
+if TYPE_CHECKING:
+    from avstack.geometry import Shape
+
+import random
 
 import numpy as np
 from avstack.config import GEOMETRY, MODELS, ConfigDict
@@ -13,17 +19,17 @@ class SensorModel:
 
     def __init__(
         self,
-        x,
-        q,
-        noise,
-        reference,
-        extent,
-        fov,
+        x: np.ndarray,
+        q: np.quaternion,
+        noise: np.ndarray,
+        reference: ReferenceFrame,
+        extent: Tuple,
+        fov: "Shape",
         ID=None,
-        Pd: float=0.95,
-        Dfa: float=1e-6,
-        p_p_FP: float=0,
-        p_p_FN: float=0,
+        Pd: float = 0.95,
+        Dfa: float = 1e-6,
+        Pp_FP: float = 0,
+        Pp_FN: float = 0,
         name="sensor",
     ) -> None:
         """Base class for an observation model
@@ -40,8 +46,8 @@ class SensorModel:
         self.q = q
         self.Pd = Pd
         self.Dfa = Dfa
-        self.p_p_FP = p_p_FP
-        self.p_p_FN = p_p_FN
+        self.Pp_FP = Pp_FP
+        self.Pp_FN = Pp_FN
         self._last_fp = []
         self._last_fn = {}
         self.extent = extent
@@ -64,42 +70,41 @@ class SensorModel:
         fov_test_objs = self.fov.check_point(obj_xs.T)
         objs_in_view = [obj for obj, in_fov in zip(objects, fov_test_objs) if in_fov]
         IDs = {obj.ID for obj in objs_in_view}
-        
+
         # remove false negatives with persistence
         self._last_fn = {ID for ID in self._last_fn if ID in IDs}
-        n_fn_persist = np.random.binomial(n=len(self._last_fn), p=self.p_p_FN)
-        n_fn_new = max(0, np.random.binomial(len(objs_in_view)-n_fn_persist, p=1-self.p_D))
-        fns_persist = np.random.choice(self._last_fn, size=n_fn_persist, replace=False)
-        fns_new = np.random.choice(IDs.difference(fns_persist), size=n_fn_new, replace=False)
+        n_fn_persist = np.random.binomial(n=len(self._last_fn), p=self.Pp_FN)
+        n_fn_new = max(
+            0, np.random.binomial(len(objs_in_view) - n_fn_persist, p=1 - self.Pd)
+        )
+        if len(self._last_fn) > 0:
+            fns_persist = random.sample(
+                self._last_fn,
+                k=n_fn_persist,
+            )
+        else:
+            fns_persist = []
+        fns_new = list(random.sample(list(IDs.difference(fns_persist)), k=n_fn_new))
         fns = fns_persist + fns_new
         for idx in sorted(np.argwhere(IDs == fns), reverse=True):
             del objs_in_view[idx]
 
         # add false positives with persistence
-        n_fp_persist = np.random.binomial(n=len(self._last_fp), p=self.p_p_FP)
-        n_fp_new = max(0, np.random.poisson(self.lambda_fp(self.fov)) - n_fp_persist)
-        fps_persist = np.random.choice(self._last_fp, size=n_fp_persist, replace=False)
-        fps_new = [Position(self.fov.sample_point(), self._reference) for _ in range(n_fp_new)]
+        n_fp_persist = np.random.binomial(n=len(self._last_fp), p=self.Pp_FP)
+        n_fp_new = max(0, np.random.poisson(self.Dfa * self.fov.area) - n_fp_persist)
+        # TODO: "could not convert object to sequence" error
+        fps_persist = list(random.sample(self._last_fp, k=n_fp_persist))
+        fps_new = [
+            Position(self.fov.sample_point(), self._reference) for _ in range(n_fp_new)
+        ]
         fps = fps_persist + fps_new
         objs_in_view += fps
         self._last_fp = fps
 
-        # # -- add false positives via rejection sampling on a rectangle
-        # n_fp = np.random.poisson(self.Dfa * self._fov_area_uniform)
-        # x = np.random.uniform(low=-self.fov.radius, high=self.fov.radius, size=n_fp)
-        # y = np.random.uniform(low=-self.fov.radius, high=self.fov.radius, size=n_fp)
-        # z = np.zeros((n_fp,))
-        # vs = np.column_stack((x, y, z))
-        # if len(vs) > 0:
-        #     fov_test_fps = self.fov.check_point(vs.T)
-        #     objs_fp = [Position(v, self._reference) for v in vs[fov_test_fps, :]]
-        # else:
-        #     objs_fp = []
-
-        # -- make measurements
+        # make measurements
         detections = []
-        for obj in enumerate(objs_in_view):
-            detections.append(self.observe(self, obj))
+        for obj in objs_in_view:
+            detections.append(self.observe(obj))
         return DataContainer(frame, timestamp, detections, self.ID)
 
     def observe(self, sensor, obj, noisy):
@@ -117,22 +122,35 @@ class PositionSensor(SensorModel):
         noise: np.ndarray = np.zeros((3,)),
         fov: ConfigDict = {"type": "Circle", "radius": 20},
         ID=None,
-        Pd=0.95,
-        Dfa=1e-6,
+        Pd: float = 0.95,
+        Dfa: float = 1e-6,
+        Pp_FP: float = 0,
+        Pp_FN: float = 0,
         name: str = "sensor",
     ) -> None:
         super().__init__(
-            x, q, noise, reference, extent, fov, ID=ID, Pd=Pd, Dfa=Dfa, name=name
+            x,
+            q,
+            noise,
+            reference,
+            extent,
+            fov,
+            ID=ID,
+            Pd=Pd,
+            Dfa=Dfa,
+            Pp_FP=Pp_FP,
+            Pp_FN=Pp_FN,
+            name=name,
         )
 
-    def observe(self, sensor, obj, noisy=True):
+    def observe(self, obj, noisy=True):
         """Make local observation and add Gaussian noise"""
         if not isinstance(obj, Position):
             obj = obj.position
         obj = obj.change_reference(self.as_reference(), inplace=False)
         xyz = obj.x + self.noise * np.random.randn(3) if noisy else obj.x
         detection = CentroidDetection(
-            source_identifier=sensor.ID,
+            source_identifier=self.ID,
             centroid=xyz,
             reference=self.as_reference(),
         )
