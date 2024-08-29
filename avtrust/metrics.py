dcd8faf50ec0f04fc31637450349022d2a0d0871
spencer@primus <spencer.hallyburton@duke.edu>
2024-08-29 12:27:54 -0400

    Add agent trust metric

--- avtrust/metrics.py
+++ avtrust/metrics.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, NamedTuple
+from typing import TYPE_CHECKING, Dict, NamedTuple
 
 
 if TYPE_CHECKING:
@@ -6,21 +6,117 @@ if TYPE_CHECKING:
     from avtrust.distributions import TrustDistribution
 
 import numpy as np
+from avstack.metrics import ConfusionMatrix
 from avstack.modules.assignment import build_A_from_distance, gnn_single_frame_assign
 
 from .distributions import TrustArray
 
 
-class MetricsAgentTrust(NamedTuple):
-    """Container for metrics on agent trust"""
+class AggregateAgentTrustMetric:
+    def __init__(self, agent_metrics: Dict[int, "AgentTrustMetric"]):
+        self.agent_metrics = agent_metrics
+
+    def __getitem__(self, key: int) -> "AgentTrustMetric":
+        return self.agent_metrics[key]
+
+    @property
+    def mean_metric(self):
+        """Try to maximize this across all agents"""
+        return np.mean([metric.metric for metric in self.agent_metrics.values()])
 
 
-class MetricsTrackTrust(NamedTuple):
-    """Container for metrics on track trust"""
+class AggregateTrackTrustMetric:
+    def __init__(self, track_metrics: Dict[int, "TrackTrustMetric"]):
+        self.track_metrics = track_metrics
 
-    m_area_below_assigned: float
-    m_area_above_unassigned: float
-    n_unassigned_truths: int
+    def __getitem__(self, key: int) -> "TrackTrustMetric":
+        return self.track_metrics[key]
+
+    @property
+    def mean_metric(self):
+        """Mix both good and bad tracks and try to maximize"""
+        return np.mean([metric.metric for metric in self.track_metrics.values()])
+
+    @property
+    def mean_area_above_cdf_assigned(self):
+        """For good tracks, try to maximize this"""
+        return np.mean(
+            [
+                metric.metric
+                for metric in self.track_metrics.values()
+                if metric.assigned_to_truth
+            ]
+        )
+
+    @property
+    def mean_area_below_cdf_unassigned(self):
+        """For bad tracks, to maximize this"""
+        return np.mean(
+            [
+                metric.metric
+                for metric in self.track_metrics.values()
+                if not metric.assigned_to_truth
+            ]
+        )
+
+    @property
+    def n_assigned_tracks(self):
+        """Number of assigned tracks"""
+        return sum([metric.assigned_to_truth for metric in self.track_metrics.values()])
+
+    @property
+    def n_unassigned_tracks(self):
+        """Number of unassigned tracks"""
+        return sum(
+            [not metric.assigned_to_truth for metric in self.track_metrics.values()]
+        )
+
+
+class AgentTrustMetric(NamedTuple):
+    """Container for metric on agent trust
+
+    f1_score: float --> good agent maximizes this
+    area_above_cdf: float --> area based on trust cdf curve
+    f1_threshold: float --> threshold on what is a "trustworthy"
+
+    if f1 < threshold, call the agent untrustworthy, so try
+    to maximize area below the CDF. if f1 >= threshold, call
+    the agent trustworthy, so try to maximize the area above CDF
+    """
+
+    f1_score: float
+    area_above_cdf: float
+    f1_threshold: float = 0.9
+
+    @property
+    def metric(self) -> float:
+        """Try to maximize this metric. Max value is 1.0"""
+        return (
+            self.area_above_cdf
+            if self.f1_score >= self.f1_threshold
+            else 1 - self.area_above_cdf
+        )
+
+
+class TrackTrustMetric(NamedTuple):
+    """Container for metrics on a single track trust
+
+    area_above_cdf: float
+    assigned_to_truth: bool
+
+    if assigned to truth, try to maximize area above
+    cdf, if not assinged to truth, try to maximize area
+    below the cdf
+    """
+
+    area_above_cdf: float
+    assigned_to_truth: bool
+
+    @property
+    def metric(self) -> float:
+        return (
+            self.area_above_cdf if self.assigned_to_truth else 1 - self.area_above_cdf
+        )
 
 
 def area_below_cdf(trust: "TrustDistribution") -> float:
@@ -35,30 +131,36 @@ def area_above_cdf(trust: "TrustDistribution") -> float:
 
 
 def get_trust_agents_metrics(
-    truths: "DataContainer",
-    tracks_agent: "DataContainer",
-    tracks_cc: "DataContainer",
+    truths_agent: Dict[int, "DataContainer"],
+    tracks_agent: Dict[int, "DataContainer"],
     trust_agents: "TrustArray",
-    trust_tracks: "TrustArray",
     assign_radius: float = 2.0,
-) -> MetricsAgentTrust:
+    f1_threshold: float = 0.9,
+) -> AggregateAgentTrustMetric:
     """Get metrics for the trust scores on agents"""
 
-    # assign agent tracks to truths
-    A_truth = build_A_from_distance(tracks_agent, truths)
-    assigns_truth = gnn_single_frame_assign(A_truth, cost_threshold=assign_radius)
+    # assign agent tracks to agent viewable truths
+    agent_metrics = {}
+    for ID_agent in truths_agent:
+        A_truth = build_A_from_distance(tracks_agent[ID_agent], truths_agent[ID_agent])
+        assigns = gnn_single_frame_assign(A_truth, cost_threshold=assign_radius)
 
-    # assign agent tracks to cc
-    A_cc = build_A_from_distance(tracks_agent, tracks_cc)
-    assigns_cc = gnn_single_frame_assign(A_cc, cost_threshold=assign_radius)
+        # build confusion matrix
+        confusion = ConfusionMatrix(
+            n_true_positives=len(assigns),
+            n_true_negatives=0,  # because assignment problem
+            n_false_positives=len(assigns.unassigned_rows),
+            n_false_negatives=len(assigns.unassigned_cols),
+        )
 
-    # (1) check trust on agent tracks assigned to cc
+        # get the metric
+        agent_metrics[ID_agent] = AgentTrustMetric(
+            f1_score=confusion.f1_score,
+            area_above_cdf=area_above_cdf(trust_agents[ID_agent]),
+            f1_threshold=f1_threshold,
+        )
 
-    # (2) tracks in agents not assign to truths
-
-    # (3) penalize for unassigned truths
-    # TODO: don't know how to do this properly yet....
-    return None
+    return AggregateAgentTrustMetric(agent_metrics)
 
 
 def get_trust_tracks_metrics(
@@ -66,28 +168,20 @@ def get_trust_tracks_metrics(
     tracks_cc: "DataContainer",
     trust_tracks: "TrustArray",
     assign_radius: float = 2.0,
-) -> MetricsTrackTrust:
+) -> AggregateTrackTrustMetric:
     """Get metrics for the trust scores on tracks"""
 
     # assign cc tracks to truths
     A = build_A_from_distance(tracks_cc, truths)
     assigns = gnn_single_frame_assign(A, cost_threshold=assign_radius)
-
-    # get the tracks that were assigned to truths
     IDs_assign = [tracks_cc[j_track].ID for j_track in assigns._row_to_col]
-    IDs_unassign = [tracks_cc[j_track].ID for j_track in assigns.unassigned_rows]
 
-    # (1) check the trust of the assigned tracks
-    area_below_assigned = [area_below_cdf(trust_tracks[ID]) for ID in IDs_assign]
+    # get the metric for each track
+    track_metrics = {}
+    for track in tracks_cc:
+        track_metrics[track.ID] = TrackTrustMetric(
+            area_above_cdf=area_above_cdf(trust_tracks[track.ID]),
+            assigned_to_truth=(track.ID in IDs_assign),
+        )
 
-    # (2) check the trust of the unassigned tracks
-    area_above_unassigned = [area_above_cdf(trust_tracks[ID]) for ID in IDs_unassign]
-
-    # (3) penalize for unassigned truths
-    n_unassigned_truths = len(assigns.unassigned_cols)
-
-    return MetricsTrackTrust(
-        m_area_below_assigned=np.mean(area_below_assigned),
-        m_area_above_unassigned=np.mean(area_above_unassigned),
-        n_unassigned_truths=n_unassigned_truths,
-    )
+    return AggregateTrackTrustMetric(track_metrics)
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -3,7 +3,12 @@ from typing import NamedTuple
 import numpy as np
 
 from avtrust.distributions import TrustArray, TrustBetaDistribution
-from avtrust.metrics import area_above_cdf, area_below_cdf, get_trust_tracks_metrics
+from avtrust.metrics import (
+    area_above_cdf,
+    area_below_cdf,
+    get_trust_agents_metrics,
+    get_trust_tracks_metrics,
+)
 
 
 def test_area_uniform():
@@ -35,21 +40,26 @@ class TestObject(NamedTuple):
         return np.linalg.norm(self.x - other.x)
 
 
-def test_get_trust_tracks_metrics_all_assigned():
+def _get_truths_and_tracks(rng, n_truths, n_fn, n_fp):
+    truths = [TestObject(x=100 * rng.randn(3), ID=i) for i in range(n_truths)]
+    truths.extend(
+        [TestObject(x=100 * rng.randn(3), ID=i + n_truths) for i in range(n_fn)]
+    )
+    tracks = [TestObject(truth.x, truth.ID) for truth in truths[:n_truths]]
+    tracks.extend(
+        [TestObject(x=100 * rng.randn(3), ID=i + n_truths) for i in range(n_fp)]
+    )
+    return truths, tracks
+
+
+def test_get_trust_tracks_metrics():
     # get some truths and tracks
     seed = 0
     rng = np.random.RandomState(seed=seed)
     n_truths = 10
     n_fn = 2
     n_fp = 1
-    truths = [TestObject(x=100 * rng.randn(3), ID=i) for i in range(n_truths)]
-    truths.extend(
-        [TestObject(x=100 * rng.randn(3), ID=i + n_truths) for i in range(n_fn)]
-    )
-    tracks_cc = [TestObject(truth.x, truth.ID) for truth in truths[:n_truths]]
-    tracks_cc.extend(
-        [TestObject(x=100 * rng.randn(3), ID=i + n_truths) for i in range(n_fp)]
-    )
+    truths, tracks_cc = _get_truths_and_tracks(rng, n_truths, n_fn, n_fp)
 
     # trust for the correct assignments
     trust_tracks = TrustArray(
@@ -82,12 +92,49 @@ def test_get_trust_tracks_metrics_all_assigned():
     )
 
     # run the metrics evaluation
-    metrics = get_trust_tracks_metrics(
+    track_metrics = get_trust_tracks_metrics(
         truths=truths, tracks_cc=tracks_cc, trust_tracks=trust_tracks, assign_radius=2.0
     )
 
     # this should result in small maba
-    assert metrics.m_area_below_assigned < 0.2
-    assert metrics.m_area_above_unassigned < 0.2
-    assert metrics.n_unassigned_truths == n_fn
-    assert not np.isnan(metrics.m_area_above_unassigned)
+    assert track_metrics.mean_area_above_cdf_assigned > 0.8
+    assert track_metrics.mean_area_below_cdf_unassigned > 0.8
+    assert track_metrics.n_assigned_tracks == n_truths
+    assert track_metrics.n_unassigned_tracks == n_fp
+
+
+def test_get_agent_tracks_metrics():
+    # get some truths and tracks
+    seed = 0
+    rng = np.random.RandomState(seed=seed)
+    n_truths = 10
+    n_fn = {0: 0, 1: 0, 2: 4, 3: 3}
+    n_fp = {0: 0, 1: 4, 2: 0, 3: 3}
+    n_agents = len(n_fn)
+    trust_agents = TrustArray(
+        timestamp=0.0,
+        trusts=[
+            TrustBetaDistribution(
+                timestamp=0,
+                identifier=i,
+                alpha=6,
+                beta=1,
+            )
+            for i in range(n_agents)
+        ],
+    )
+
+    # build tracks for each agent
+    truths_agents = {}
+    tracks_agents = {}
+    for ID_agent in range(n_agents):
+        truths_agents[ID_agent], tracks_agents[ID_agent] = _get_truths_and_tracks(
+            rng, n_truths, n_fn[ID_agent], n_fp[ID_agent]
+        )
+
+    # compute metrics
+    agent_metrics = get_trust_agents_metrics(truths_agents, tracks_agents, trust_agents)
+
+    # only agent 0 should have a good overall metric
+    assert agent_metrics[0].metric > 0.7
+    assert all([agent_metrics[ID_agent].metric < 0.4 for ID_agent in [1, 2, 3]])
